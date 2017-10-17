9f3e4e4051cda3fc32ca2e7ca4a79fb2e8f011c7
Nan Li <n69li@uwaterloo.ca>
2017-10-17 12:09:04 -0400

    add

---
+++ src/main/java/com/ece651/handymenserver/RestSvr/UsrService.java
@@ -0,0 +1,9 @@
+package com.ece651.handymenserver.RestSvr;
+
+public class UsrService 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
